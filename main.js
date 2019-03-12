63ba7363cf11fea59957e72075ae641fca7cf7c1
gabrielblins <academic.gabrielblins@gmail.com>
2019-03-12 14:50:50 -0300

    Update on main script file: On click the color of square changes

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 //Variables
-var bombs, GraySquares = [], WhiteSquareX = [], WhitheSquareY = [], WhiteSquaresCounter = 0,
-x = 0, y = 0;
+var bombs, GraySquares = [], WhiteSquareX = [], WhitheSquareY = [], WhiteSquaresCounter = 0, WhiteSquareIsClicked = [], 
+mouseIsClicked = false, x = 40, y = 40;
 
 
 function setup() {
 
     createCanvas(641,641);
     background(255,186,120);
+    rectMode(CENTER);
 
     for(i = 0; i<8; i++){
         for(j = 0; j<8; j++){
@@ -14,19 +15,35 @@ function setup() {
             WhiteSquareX[WhiteSquaresCounter] = x;
             WhitheSquareY[WhiteSquaresCounter] = y;
             x+=80;
-            if(x==640)
-                x = 0;
+            if(x > 600)
+                x = 40;
             WhiteSquaresCounter++;
-
+            
         }
-        
-        y+=80;
+
+            y+=80;
 
     }
 
+    for(k = 0; k < 64; k++)
+        WhiteSquareIsClicked[k] = false;
+
+}
+
+function draw() {
+
     for(WhiteSquaresCounter = 0; WhiteSquaresCounter < 64; WhiteSquaresCounter++){
-        fill(232);
+
+        if(mouseIsPressed && dist(mouseX,mouseY,WhiteSquareX[WhiteSquaresCounter],WhitheSquareY[WhiteSquaresCounter]) < 40)
+            WhiteSquareIsClicked[WhiteSquaresCounter] = true;
+        
+        if(WhiteSquareIsClicked[WhiteSquaresCounter])
+            fill(132);
+        else
+            fill(232);
+
         rect(WhiteSquareX[WhiteSquaresCounter],WhitheSquareY[WhiteSquaresCounter],80,80);
+
     }
 
 }
